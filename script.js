27afddc9ed38eba23f4c018e8075ae74566d71e1
Pedro Lucas <izielucas2016@gmail.com>
2025-03-27 12:46:21 -0300

    FEAT/Created IDs and imported in JS by DOM

---
+++ script.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById('btn');
+const mortAmo = document.getElementById('mortAmo').value;
+const mortTer = document.getElementById('mortTer').value;
+const inteRat = document.getElementById('inteRat').value;
+const repay = document.getElementById('repay').checked;
+const inteOnl = document.getElementById('inteOnl').checked;
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+})
