30b39aa3945b4a5e24657d65a64e27c08e41e714
Pedro Lucas <izielucas2016@gmail.com>
2025-03-27 13:31:57 -0300

    Created switch error to results

--- script.js
+++ script.js
@@ -4,8 +4,22 @@ const mortTer = document.getElementById('mortTer').value;
 const inteRat = document.getElementById('inteRat').value;
 const repay = document.getElementById('repay').checked;
 const inteOnl = document.getElementById('inteOnl').checked;
+const empty = document.getElementById('empty');
+const containRes = document.getElementById('res');
+const monthRep = document.getElementById('monthRep');
+const repOve = document.getElementById('repOve');
+
+const juros = (mont, juros, tempo) => {
+    return mont/Math.pow((1+(juros/100)), tempo)
+}
 
 btn.addEventListener('click', (e) => {
     e.preventDefault();
-    
+
+    monthRep.textContent = 
+    repOve.textContent = juros(mortAmo, inteRat, mortTer).toFixed(2)
+
+    empty.style.display = 'none'
+    containRes.style.display = 'flex';
+
 })
